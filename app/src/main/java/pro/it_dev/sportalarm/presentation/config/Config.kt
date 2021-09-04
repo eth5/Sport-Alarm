c6602b14538dc0f5b264f902267e758bd67e6ccb
xWeb <29051616+eth5@users.noreply.github.com>
2021-09-05 00:04:21 +1000

    Refactor (strings to res)

--- app/src/main/java/pro/it_dev/sportalarm/presentation/config/Config.kt
+++ app/src/main/java/pro/it_dev/sportalarm/presentation/config/Config.kt
@@ -4,11 +4,14 @@ import android.widget.Toast
 import androidx.compose.animation.core.Spring
 import androidx.compose.animation.core.animateFloatAsState
 import androidx.compose.animation.core.spring
-import androidx.compose.foundation.*
+import androidx.compose.foundation.background
+import androidx.compose.foundation.border
 import androidx.compose.foundation.layout.*
+import androidx.compose.foundation.rememberScrollState
 import androidx.compose.foundation.shape.CircleShape
 import androidx.compose.foundation.shape.RoundedCornerShape
 import androidx.compose.foundation.text.KeyboardOptions
+import androidx.compose.foundation.verticalScroll
 import androidx.compose.material.*
 import androidx.compose.runtime.*
 import androidx.compose.ui.Alignment
@@ -30,24 +33,23 @@ import androidx.compose.ui.window.Dialog
 import androidx.compose.ui.window.DialogProperties
 import androidx.lifecycle.viewmodel.compose.viewModel
 import pro.it_dev.sportalarm.R
-import pro.it_dev.sportalarm.presentation.sound.Sound
 import pro.it_dev.sportalarm.util.Message
 import pro.it_dev.sportalarm.util.Resource
 
 //todo Add soundPlayer by di for test play
 @Composable
-fun ConfigDialog(viewModel: ConfigViewModel = viewModel(), onDismissRequest:()->Unit) {
+fun ConfigDialog(viewModel: ConfigViewModel = viewModel(), onDismissRequest: () -> Unit) {
 	Dialog(
 		onDismissRequest = onDismissRequest,
 		properties = DialogProperties(dismissOnClickOutside = true)
-	){
-		Box(modifier = Modifier
-			.fillMaxHeight(0.8f)
-			.fillMaxWidth(1f)
-			.border(1.dp, MaterialTheme.colors.secondary, RoundedCornerShape(10.dp))
-			,
+	) {
+		Box(
+			modifier = Modifier
+				.fillMaxHeight(0.8f)
+				.fillMaxWidth(1f)
+				.border(1.dp, MaterialTheme.colors.secondary, RoundedCornerShape(10.dp)),
 			contentAlignment = Center
-		){
+		) {
 			var size by remember {
 				mutableStateOf(0f)
 			}
@@ -57,11 +59,12 @@ fun ConfigDialog(viewModel: ConfigViewModel = viewModel(), onDismissRequest:()->
 					dampingRatio = Spring.DampingRatioHighBouncy
 				)
 			)
-			LaunchedEffect(Unit, block = {size=1f})
-			Column(modifier = Modifier
-				.fillMaxSize(animation)
-				.background(color = MaterialTheme.colors.background, RoundedCornerShape(10.dp))
-				.shadow(0.dp, RoundedCornerShape(10.dp))
+			LaunchedEffect(Unit, block = { size = 1f })
+			Column(
+				modifier = Modifier
+					.fillMaxSize(animation)
+					.background(color = MaterialTheme.colors.background, RoundedCornerShape(10.dp))
+					.shadow(0.dp, RoundedCornerShape(10.dp))
 			) {
 				Text(
 					text = "Config",
@@ -73,7 +76,7 @@ fun ConfigDialog(viewModel: ConfigViewModel = viewModel(), onDismissRequest:()->
 						.padding(start = 5.dp)
 						.align(CenterHorizontally)
 				)
-				val clockState by produceState<Resource<Boolean>>(initialValue = Resource.Loading()){
+				val clockState by produceState<Resource<Boolean>>(initialValue = Resource.Loading()) {
 					value = viewModel.getClock()
 				}
 				Box(
@@ -83,18 +86,22 @@ fun ConfigDialog(viewModel: ConfigViewModel = viewModel(), onDismissRequest:()->
 						.padding(start = 16.dp, end = 16.dp),
 					contentAlignment = Alignment.Center
 
-				){
-					when(clockState){
+				) {
+					when (clockState) {
 						is Resource.Loading -> CircularProgressIndicator()
 						is Resource.Success -> ConfigSetting(viewModel)
-						is Resource.Error -> Text(text = clockState.message ?: "Unknown error", color = Color.Red)
+						is Resource.Error -> Text(
+							text = clockState.message ?: "Unknown error",
+							color = Color.Red
+						)
 					}
 				}
 
-				Row (modifier = Modifier
-					.fillMaxWidth()
-					.padding(10.dp)
-				){
+				Row(
+					modifier = Modifier
+						.fillMaxWidth()
+						.padding(10.dp)
+				) {
 					TextButton(
 						onClick = {
 							viewModel.saveClock()
@@ -125,7 +132,7 @@ fun ToastMessage(messageState: MutableState<Message?>) {
 	val message by remember { messageState }
 	if (message != null) {
 		val ctx = LocalContext.current
-		DisposableEffect(message){
+		DisposableEffect(message) {
 			val toast = Toast.makeText(ctx, message?.text ?: "error msg", Toast.LENGTH_SHORT)
 			toast.show()
 			messageState.value = null
@@ -146,14 +153,14 @@ fun ConfigSetting(viewModel: ConfigViewModel) {
 		verticalArrangement = Arrangement.Center
 	) {
 		val ctx = LocalContext.current
-		Box (
+		Box(
 			modifier = Modifier
 				.padding(horizontal = 2.dp, vertical = 10.dp)
 				.border(1.dp, MaterialTheme.colors.secondary, CircleShape)
 				.shadow(2.dp, CircleShape)
 				.background(color = MaterialTheme.colors.background, CircleShape),
 			contentAlignment = Center
-		){
+		) {
 
 			val volume by remember { viewModel.volume }
 			Slider(
@@ -175,7 +182,7 @@ fun ConfigSetting(viewModel: ConfigViewModel) {
 			}
 		)
 		Text(text = LocalContext.current.getString(R.string.work_time))
-		Row (verticalAlignment = Alignment.CenterVertically){
+		Row(verticalAlignment = Alignment.CenterVertically) {
 			EditField(
 				stateValue = viewModel.min,
 				label = { Text(text = ctx.getString(R.string.min)) },
@@ -197,13 +204,16 @@ fun ConfigSetting(viewModel: ConfigViewModel) {
 			)
 		}
 		Text(text = LocalContext.current.getString(R.string.pause_time))
-		Row (verticalAlignment = Alignment.CenterVertically){
+		Row(verticalAlignment = Alignment.CenterVertically) {
 			EditField(
 				stateValue = viewModel.pauseMin,
 				label = { Text(text = ctx.getString(R.string.min)) },
 				modifier = Modifier.weight(1f),
 				keyboardOptions = KeyboardOptions(keyboardType = KeyboardType.Number),
-				onChangeSetter = { if (it.length < 9) viewModel.pauseMin.value = viewModel.convertStringValueToLong(it) }
+				onChangeSetter = {
+					if (it.length < 9) viewModel.pauseMin.value =
+						viewModel.convertStringValueToLong(it)
+				}
 			)
 			Text(text = ":")
 			EditField(
@@ -211,7 +221,10 @@ fun ConfigSetting(viewModel: ConfigViewModel) {
 				label = { Text(text = ctx.getString(R.string.sec)) },
 				modifier = Modifier.weight(1f),
 				keyboardOptions = KeyboardOptions(keyboardType = KeyboardType.Number),
-				onChangeSetter = { if (it.length < 9) viewModel.pauseSec.value = viewModel.convertStringValueToLong(it) }
+				onChangeSetter = {
+					if (it.length < 9) viewModel.pauseSec.value =
+						viewModel.convertStringValueToLong(it)
+				}
 			)
 		}
 
@@ -241,7 +254,12 @@ fun ConfigSetting(viewModel: ConfigViewModel) {
 }
 
 @Composable
-fun LabeledCheckBox(value:State<Boolean>, label:String, modifier: Modifier = Modifier, onChange:(Boolean)->Unit) {
+fun LabeledCheckBox(
+	value: State<Boolean>,
+	label: String,
+	modifier: Modifier = Modifier,
+	onChange: (Boolean) -> Unit
+) {
 	Row(modifier = modifier) {
 		val isChecked by remember { value }
 		Checkbox(checked = isChecked, onCheckedChange = { onChange(it) })
@@ -250,18 +268,17 @@ fun LabeledCheckBox(value:State<Boolean>, label:String, modifier: Modifier = Mod
 }
 
 @Composable
-fun <T>EditField(
-	stateValue:State<T>,
-	adapter:(T)->String = { it.toString() },
-	label: @Composable (() -> Unit)? = null,
+fun <T> EditField(
+	stateValue: State<T>,
 	modifier: Modifier = Modifier,
+	adapter: (T) -> String = { it.toString() },
+	label: @Composable (() -> Unit)? = null,
 	fontSize: TextUnit = 18.sp,
-	maxLines:Int = 1,
-	keyboardOptions: KeyboardOptions = KeyboardOptions (),
-	onChangeSetter: (String)->Unit
-){
+	maxLines: Int = 1,
+	keyboardOptions: KeyboardOptions = KeyboardOptions(),
+	onChangeSetter: (String) -> Unit
+) {
 	val stateValue by remember { stateValue }
-	var size by remember { mutableStateOf(0.8f) }
 	var borderSize by remember { mutableStateOf(1.dp) }
 
 	TextField(
@@ -274,15 +291,12 @@ fun <T>EditField(
 			.shadow(2.dp, CircleShape)
 			.background(color = MaterialTheme.colors.background, CircleShape)
 			.onFocusChanged {
-				if (it.isFocused) {
-					size = 0.9f
-					borderSize = 3.dp
+				borderSize = if (it.isFocused) {
+					3.dp
 				} else {
-					size = 0.8f
-					borderSize = 1.dp
+					1.dp
 				}
-			}
-		,
+			},
 		maxLines = maxLines,
 		textStyle = TextStyle(
 			fontSize = fontSize
